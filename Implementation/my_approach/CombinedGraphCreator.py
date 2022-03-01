3b1b4bb7927ef4d7e95d0f2d356b90dc8152e818
Christoph <chschaef@mail.uni-paderborn.de>
2022-03-01 10:25:26 +0100

    created combined figures for pretraining umls, all models

--- Implementation/my_approach/CombinedGraphCreator.py
+++ Implementation/my_approach/CombinedGraphCreator.py
@@ -2,12 +2,11 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
 class CombinedGraphCreator():
-  def __init__(self, dataset, models, n_epochs, train_type, sampling_type = "", logs = []):
+  def __init__(self, dataset, models, n_epochs, train_type, sampling_type = ""):
     self.dataset = dataset
-    self.models = models # list with models, one model for pretraining and two models gan_train (for generator and discriminator)
+    self.models = models # list with models, pretrain: list of models, gan_train: list of lists with models of generator and discriminator
     self.train_type = train_type
     self.sampling_type = sampling_type
-    self.logs = logs
     self.n_epochs = n_epochs
     
    
@@ -25,22 +24,26 @@ class CombinedGraphCreator():
     
     plt.grid(True)  
     return plt
+  
+   
+   
+  def get_values(self, value_name, line):
+    return list(map(float, line.replace(value_name + " = ", "").replace("\n", "").replace("[", "").replace("]", "").split(",")))   
    
   def create_combined_graph(self):
     # init arrays with logged values
     logged_mrrs, logged_hits10s, logged_rewards, logged_losses = dict(), dict(), dict(), dict()
     
     # read all files
-    for i in range(len(self.logs)):
-      log = self.logs[i]
+    for i in range(len(self.models)):
       models = self.models[i]
-      dir = "logs/"
+      dir = "logs/backup/"
       model_name = None
       if self.train_type == "pretrain":
-        dir += log + "_" + self.train_type + "_" +  models.lower()  + "_" + self.dataset 
+        dir += self.train_type + "/" +  self.dataset + "/" + models.lower() 
         model_name = models.lower()
       elif self.train_type == "gan_train":
-        dir += log + "_" + self.train_type + "_" + models[0].lower() +  "_" + models[1].lower()  + "_" + self.dataset 
+        dir += self.train_type + "/" + self.sampling_type  + "/" + self.dataset + "/" + models[0].lower() +  "_" + models[1].lower()
         model_name = models[0].lower() + " + " + models[1].lower()
         
       mrrs, hits10s, rewards, losses = [], [], [], []      
@@ -48,49 +51,44 @@ class CombinedGraphCreator():
         lines = f.readlines()
         for line in lines:          
           if line.startswith('rewards') and self.train_type == "gan_train":
-            rewards = line.replace("rewards = ", "").replace("\n", "").replace("[", "").replace("]", "")
-            rewards = rewards.split(",")            
-            rewards = list(map(float, rewards))
+            rewards = self.get_values("rewards", line)
           elif line.startswith('losses'):
-            losses = line.replace("losses = ", "").replace("\n", "").replace("[", "").replace("]", "")
-            losses = losses.split(",")            
-            losses = list(map(float, losses))
+            losses = self.get_values("losses", line)
           elif line.startswith('mrrs'):
-            mrrs = line.replace("mrrs = ", "").replace("\n", "").replace("[", "").replace("]", "")
-            mrrs = mrrs.split(",")            
-            mrrs = list(map(float, mrrs))
+            mrrs = self.get_values("mrrs", line)
           elif line.startswith('hits10s'):
-            hits10s = line.replace("hits10s = ", "").replace("\n", "").replace("[", "").replace("]", "")
-            hits10s = hits10s.split(",")            
-            hits10s = list(map(float, hits10s))
+            hits10s = self.get_values("hits10s", line)
              
         logged_mrrs[model_name] = mrrs
         logged_hits10s[model_name] = hits10s
         logged_rewards[model_name]  = rewards
         logged_losses[model_name] = losses   
-             
+         
+    if self.train_type == "pretrain":
+      dir = 'combined_figures/' + self.train_type + '/' + self.dataset + '/'
+    elif self.train_type == "gan_train":    
+      dir = 'combined_figures/' + self.train_type + '/' + self.sampling_type + '/' + self.dataset + '/'
     self.create_figure(title = self.train_type + " " + self.dataset.upper() + " Validation MRR " + self.sampling_type, 
                        logged_values = logged_mrrs, 
-                       y_label = "MRR").savefig('combined_figures/' + self.train_type + '_' + self.dataset + '_combined_mrrs')
+                       y_label = "MRR").savefig(dir + 'mrrs')
     self.create_figure(title = self.train_type + " " + self.dataset.upper() + " Validation H@10 " + self.sampling_type, 
                        logged_values = logged_hits10s, 
-                       y_label ="H@10").savefig('combined_figures/' + self.train_type + '_' + self.dataset + '_combined_hit10s')   
+                       y_label ="H@10").savefig(dir + 'hit10s')   
     self.create_figure(title = self.train_type + " " + self.dataset.upper() + " Validation Losses " + self.sampling_type, 
                        logged_values = logged_losses, 
-                       y_label ="Losses").savefig('combined_figures/' + self.train_type + '_' + self.dataset + '_combined_losses')
+                       y_label ="Losses").savefig(dir + 'losses')
     
     if self.train_type != "pretrain":
       self.create_figure(title = self.train_type + " " + self.dataset.upper() + " Validation Rewards " + self.sampling_type, 
                         logged_values = logged_rewards, 
-                        y_label ="Rewards").savefig('combined_figures/' + self.train_type + '_' + self.dataset + '_combined_rewards')
+                        y_label ="Rewards").savefig(dir + 'rewards')
     
     
     
-# cgc = CombinedGraphCreator(dataset = "wn18rr", 
-#                            models = ["DistMult", "TransE"], 
+# cgc = CombinedGraphCreator(dataset = "umls", 
+#                            models = ["DistMult", "ComplEx", "TransE", "TransD"], 
 #                            n_epochs= 1000,
-#                            train_type = "pretrain", 
-#                            logs = ["2022.02.28.132428", "2022.02.28.130247"])
+#                            train_type = "pretrain")
 
 # cgc.create_combined_graph()
 
@@ -100,7 +98,6 @@ cgc = CombinedGraphCreator(dataset = "wn18rr",
                            models = [["DistMult", "TransE"]], 
                            n_epochs= 5000,
                            train_type = "gan_train", 
-                           sampling_type = "Random Sampling",
-                           logs = ["2022.02.23.121626"])
+                           sampling_type = "random")
 
 cgc.create_combined_graph()
